e7b312a0c0e019abb27dfaec904b3ef01f98006e
Eric <service@furnishedhousingelite.com>
2026-03-08 16:14:11 +0800

    Remove internal files, add .gitignore

--- scripts/run_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~/amazon-autopilot
-echo "=== Amazon Autopilot 週報更新 $(date) ==="
-bash scripts/run_daiken.sh
-# bash scripts/run_dbj.sh     ← DBJ 之後加
-# bash scripts/run_flux.sh    ← Flux 之後加
-echo "=== 全部完成 ==="
